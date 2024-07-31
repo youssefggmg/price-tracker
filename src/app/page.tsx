090349e4d8effc275f10112bc8c7f3e2b2a932b6
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-07-31 18:38:24 +0100

    "Added Features component to Home page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/utils/navebar";
 import Hero from "@/components/utils/hero";
 import Stats from "@/components/utils/stats";
+import Features from "@/components/utils/Features";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Navbar/>
     <Hero/>
     <Stats/>
+    <Features/>
     </>
   );
 }
---
+++ src/components/utils/Features.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <div>
+            <h2 className='text-center roboto-black text-4xl my-12'>features that we have on our website</h2>
+        </div>
+    )
+}
+
+export default Features
